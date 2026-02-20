248c52aad8e5a1cd5dbd36015cb1ab92b414d096
Miguel11290 <miguel.bonello.e21277@mcast.edu.mt>
2026-02-20 12:48:10 +0100

    Updated the main.py

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from routes_items import router as items_router
 from routes_inventory import router as inventory_router
 from routes_safety import router as safety_router
 from routes_ml import router as ml_router
-from backend.routes_eval import router as eval_router
+from routes_eval import router as eval_router
 from routes_metrics import router as metrics_router
 import models
 
